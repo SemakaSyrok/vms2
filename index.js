1130cc53b80b8bc3e4db1bdd5d16b118d356128d
SemakaSyrok <semakasyrok1@gmail.com>
2019-06-20 13:41:57 +0700

    change client

--- index.js
+++ index.js
@@ -9,31 +9,31 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
 
-db.sync({force: true}).then(() => {
-    User.create({
-        login: "abminadmin",
-        name: "Алексей",
-        pass: "12pass32word99",
-        token: "111111111",
-        is_admin: 1
-    });
-    User.create({
-        login: "testUser",
-        name: "Алексейa",
-        pass: "testpassword",
-        token: "1111111112",
-        is_admin: 0
-    });
-    Camera.create({
-        name: 'Камера',
-        connection_string: 'http://217.197.157.7:7070/axis-cgi/mjpg/video.cgi?camera=1',
-        owner_id: 2
-    });
-    Camera.create({
-        name: 'Камера2',
-        connection_string: 'http://meteobunyol.axiscam.net:9000/mjpg/video.mjpg',
-        owner_id: 2
-    });
+db.sync({force: false}).then(() => {
+    // User.create({
+    //     login: "abminadmin",
+    //     name: "Алексей",
+    //     pass: "12pass32word99",
+    //     token: "111111111",
+    //     is_admin: 1
+    // });
+    // User.create({
+    //     login: "testUser",
+    //     name: "Алексейa",
+    //     pass: "testpassword",
+    //     token: "1111111112",
+    //     is_admin: 0
+    // });
+    // Camera.create({
+    //     name: 'Камера',
+    //     connection_string: 'http://217.197.157.7:7070/axis-cgi/mjpg/video.cgi?camera=1',
+    //     owner_id: 2
+    // });
+    // Camera.create({
+    //     name: 'Камера2',
+    //     connection_string: 'http://meteobunyol.axiscam.net:9000/mjpg/video.mjpg',
+    //     owner_id: 2
+    // });
 });
 // sequelize.sync()
 
